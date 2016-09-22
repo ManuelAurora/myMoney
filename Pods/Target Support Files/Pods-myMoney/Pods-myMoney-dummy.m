eb1bb1dcc2315857236622497cf7ae49abbd6ebe
Manuel <naruto06@mail.ru>
2016-09-22 14:19:46 +0300

    Removed: Pods

--- Pods/Target Support Files/Pods-myMoney/Pods-myMoney-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_myMoney : NSObject
-@end
-@implementation PodsDummy_Pods_myMoney
-@end
--- Pods/Target Support Files/Pods-myMoneyTests/Pods-myMoneyTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_myMoneyTests : NSObject
-@end
-@implementation PodsDummy_Pods_myMoneyTests
-@end
